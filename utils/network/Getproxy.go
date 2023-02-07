5b6c8bdac918f90d1ed5628fd0f471502af67874
stupidfish <2928109164@qq.com>
2023-02-07 14:57:03 +0800

    加入代理功能

--- utils/network/Getproxy.go
+++ utils/network/Getproxy.go
@@ -1,44 +1,37 @@
 package network
 
-import (
-	"github.com/storyicon/golang-proxy/business"
-	"log"
-	"os"
-	"tool/utils"
-)
-
 func ProxyCrawlerInit() {
-
-	if os.IsNotExist(func() error {
-		_, err := os.Stat("source")
-		return err
-	}()) {
-		err := os.Mkdir("source", os.ModePerm)
-		if err != nil {
-			println("目录创建失败")
-			os.Exit(0)
-		}
-	}
-	utils.Printminfo("代理爬虫开启")
-	var mode string
-	mode = "all"
-
-	switch mode {
-	case "all":
-		go business.StartProducer()
-		go business.StartConsumer()
-		go business.StartAssessor()
-		go business.StartService()
-	case "consumer":
-		business.StartConsumer()
-	case "producer":
-		business.StartProducer()
-	case "assessor":
-		business.StartAssessor()
-	case "service":
-		business.StartService()
-	default:
-		log.Panicf("Unknown mode: %s", mode)
-	}
-	select {}
+	//
+	//if os.IsNotExist(func() error {
+	//	_, err := os.Stat("source")
+	//	return err
+	//}()) {
+	//	err := os.Mkdir("source", os.ModePerm)
+	//	if err != nil {
+	//		println("目录创建失败")
+	//		os.Exit(0)
+	//	}
+	//}
+	//utils.Printminfo("代理爬虫开启")
+	//var mode string
+	//mode = "all"
+	//
+	//switch mode {
+	//case "all":
+	//	go business.StartProducer()
+	//	go business.StartConsumer()
+	//	go business.StartAssessor()
+	//	go business.StartService()
+	//case "consumer":
+	//	business.StartConsumer()
+	//case "producer":
+	//	business.StartProducer()
+	//case "assessor":
+	//	business.StartAssessor()
+	//case "service":
+	//	business.StartService()
+	//default:
+	//	log.Panicf("Unknown mode: %s", mode)
+	//}
+	//select {}
 }
